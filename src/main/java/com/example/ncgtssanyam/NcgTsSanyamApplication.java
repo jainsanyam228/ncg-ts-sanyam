40c9f51e53004171be0adff7f4be0420170593d8
Sanyam Jain <sanyam.jain@thoughtspot.com>
2023-07-19 16:58:01 +0530

    Initial commit

---
+++ src/main/java/com/example/ncgtssanyam/NcgTsSanyamApplication.java
@@ -0,0 +1,13 @@
+package com.example.ncgtssanyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcgTsSanyamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcgTsSanyamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ncgtssanyam/NcgTsSanyamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ncgtssanyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcgTsSanyamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
